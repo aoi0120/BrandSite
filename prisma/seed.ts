cab44ebe977781500b7a84f3a87a6a516331d4bd
AoiXD <oosabaaoi@gmail.com>
2024-11-12 03:34:08 +0900

    Prismaスキーマの更新: tsconfig.jsonにts-node設定を追加し、Expressアプリケーションを構築。ユーザー情報を取得するAPIエンドポイントを追加し、package.jsonのスクリプトを更新。seed.tsのユーザー作成ロジックを修正。

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,27 +2,29 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+
 const main = async () => {
-    const alice = await prisma.user.upsert({
-        where: { id: '101418a6-9738-40cb-8749-2439d516f31e' },
-        update: {},
-        create: {
-            name: "Alice",
-            email: "Alice@prisma.io",
-        },
-    })
+    const alice = await prisma.user.findFirst({ where: { name: 'Alice' } })
+    if (!alice) {
+        prisma.user.create({
+            data: {
+                name: 'Alice',
+                email: 'Alice@prisma.io',
+            }
+        })
+    }
 
-    const bob = await prisma.user.upsert({
-        where: { id: 'aece4598-578e-4141-a9dd-c1fd0bc97be3' },
-        update: {},
-        create: {
-            name: "Bob",
-            email: "Bob@prisma.io",
-        },
-    })
-    console.log({ alice, bob });
-
-};
+    const bob = await prisma.user.findFirst({ where: { name: 'Bob' } })
+    if (!bob) {
+        prisma.user.create({
+            data: {
+                name: 'Bob',
+                email: 'Bob@prisma.io',
+            }
+        })
+        console.log({ alice, bob });
+    };
+}
 
 main() // Run the main function
     .catch(e => {
