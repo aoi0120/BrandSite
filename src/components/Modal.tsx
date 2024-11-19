5e0a5ea38d2ab7032d0113e127abe113c7fd2fe5
AoiXD <oosabaaoi@gmail.com>
2024-11-19 17:53:21 +0900

    Modalコンポーネントに外部クリックでモーダルを閉じる機能を追加し、ブランド画像の表示を改善

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,6 +13,12 @@ export const Modal: React.FC<ModalProps> = ({ showFlag,setShowModal,selectedBran
         setShowModal(false);
     };
 
+    const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target === e.currentTarget) {
+            closeModal();
+        }
+    };
+
     const brand = brandDates.find((data) => data.name === selectedBrandName)
 
     if(!showFlag || !selectedBrandName || !brand) return null;
@@ -20,11 +26,11 @@ export const Modal: React.FC<ModalProps> = ({ showFlag,setShowModal,selectedBran
     return (
         <>
             {showFlag ? ( //ture
-                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-slate-800 bg-opacity-40">
-                    <div className="bg-white p-10 rounded-md">
+                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-slate-800 bg-opacity-40" onClick={handleOutsideClick}>
+                    <div className="bg-white p-10 rounded-md m-10">
                         <h2 className='text-lg fixed'>{brand.name}</h2>
-                        <div>
-                            
+                        <div className='w-auto h-auto'>
+                            <img  src={brand.photo_url}></img>
                         </div>
                         <button onClick={closeModal}>Close</button>
                     </div>
