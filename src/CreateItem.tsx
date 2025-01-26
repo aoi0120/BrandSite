91aa3049e6bb010b69d64826814f283e86d166b9
AoiXD <oosabaaoi@gmail.com>
2025-01-26 12:51:52 +0900

    package.jsonとpackage-lock.jsonにreact-routerを追加し、Expressサーバーに/itemsエンドポイントを実装。新しいCreateItemコンポーネントを作成し、ルーティングを設定。

---
+++ src/CreateItem.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export const CreateItem = () => {
+    const [form, setForm] = useState({
+        name: '',
+        description: '',
+        photo_url: '',
+        brand_id: '',
+        tags: '',
+    })
+
+    return(
+        <p>exit</p>
+    )
+
+}
--- src/db/app.ts
+++ src/db/app.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 const prisma = new PrismaClient();
 
 const app = express();
+const port = 8080
 
 app.use(cors());
 app.use(express.json());
@@ -29,5 +30,31 @@ app.get('/items', async (req, res) => {
     const items = await prisma.item.findMany();
     res.json(items);
 });
+
+app.post('/api/items', async (req,res) => {
+    const { name, description,photo_url,tags, brand_id} = req.body;
+
+    try {
+        const newItem = await prisma.item.create({
+            data: {
+                name,
+                description,
+                photo_url,
+                tags,
+                brand: {
+                    connect: {id: brand_id}
+                }
+            },
+        })
+        res.status(201).json(newItem);
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({error: 'Failed to create Item'});
+    }
+});
+
+app.listen(port,() => {
+    console.log("Server running")
+})
 
 export default app;
