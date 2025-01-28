8d1fcc099848fcd00195559a6ad6fe2cb66c495d
AoiXD <oosabaaoi@gmail.com>
2025-01-29 00:59:51 +0900

    不要なファイルと設定を削除

--- src/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-    return (
-        <footer className="bg-slate-600 ">
-            <div className="h-[200px]">
-                <p className="text-center pt-20">footerrrr</p>
-            </div>
-        </footer>
-    )
-}
--- src/components/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Brand {
-        id: number;
-        photo_url: string;
-        name: string;
-        name_hira: string;
-        url : string;
-}
